01e7a2df51738b3bdfc67a34a93aeb8aa5c2ff9f
NICKY-TECH <tinaonyis@gmail.com>
2024-03-18 09:59:06 +0100

    update main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import "./styles/base.css"
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import filter from "./features/preferenceOverlay.ts";
+import view from "./features/viewOverlay.ts"
+
+
+const store = configureStore({
+  reducer: {
+  filter,
+  view
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+ <Provider store={store}>
+ <App />
+ </Provider>
   </React.StrictMode>,
 )
