ef5729b7eaab1104bc90e5a479bbb1cfbab29a04
NICKY-TECH <tinaonyis@gmail.com>
2024-03-18 08:30:32 +0100

    add registratio form component

---
+++ src/Components/Registration/RegistrationForm.tsx
@@ -0,0 +1,19 @@
+import { Input,Button } from "../../index";
+import "../../styles/base.css";
+
+function RegistrationForm() {
+  return <section className="login-form">
+   <div className="login-inner-content">
+   <h1>Welcome!</h1>
+    <h2>Enter details to login.</h2>
+    <form>
+      <Input {...{ placeholder: "Email" ,type:"email"}} />
+      <Input {...{ placeholder: "Password",type:"password" }} />
+      <a href="#">FORGOT PASSWORD</a>
+      <Button {...{class:"reg-button",text:"LOG IN"}}/>
+    </form>
+   </div>
+  </section>;
+}
+
+export default RegistrationForm;
