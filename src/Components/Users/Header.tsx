22c1ed5174b77567e87033522812548f977061c1
NICKY-TECH <tinaonyis@gmail.com>
2024-03-18 08:33:40 +0100

    add header  component to user folder

---
+++ src/Components/Users/Header.tsx
@@ -0,0 +1,25 @@
+import "../../styles/base.css";
+import logo from "../../resources/images/icons/Group (1).svg";
+import {Search,SecondHalf} from "../../index";
+
+
+function Header() {
+  return (
+    <header className="header">
+      <div role="group-logo-and-input" className="logo-group">
+        <img src={logo}  className="logo"/>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 ham">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+</svg>
+
+
+    <Search/>
+      </div>
+      <div role="group-left-items" className="header-section-two">
+        <SecondHalf/>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
