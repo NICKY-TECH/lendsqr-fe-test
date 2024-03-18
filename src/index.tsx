bed9577413410842bf51a433eefd2b98019717fa
NICKY-TECH <tinaonyis@gmail.com>
2024-03-18 10:00:38 +0100

    update index.ts

---
+++ src/index.tsx
@@ -0,0 +1,39 @@
+import Input from "./Components/Registration/Input";
+import Button from "./Components/Registration/Button";
+import RegistrationForm from "./Components/Registration/RegistrationForm";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import MainBody from './Components/Users/MainBody';
+import Header from "./Components/Users/Header";
+import Search from './Components/Users/Search';
+import SecondHalf from './Components/Users/SecondHalf';
+import Card from './Components/Users/Card';
+import SideBar from './Components/Users/SideBar';
+import SideBarList from './Components/Users/SideBarList';
+import MobileFooterBar from './Components/Users/MobileFooterBar';
+import DashBoardButton from './Components/Users/Button';
+import Paginate from './Components/Users/Paginate';
+import Selection from './Components/Users/Selection';
+import InputWrapper from './Components/Users/InputWrapper';
+import FilterIcon from './Components/Users/FilterIcon';
+
+export {
+  Input,
+  Button,
+  Selection,
+  RegistrationForm,
+  InputWrapper,
+  FilterIcon,
+  MobileFooterBar,
+  DashBoardButton,
+  Paginate,
+  Card,
+  Dashboard,
+  SecondHalf,
+  Header,
+  Search,
+  SideBar,
+  SideBarList,
+  MainBody,
+  Login
+};
